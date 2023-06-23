bb44c2f9b88280cd9ca898c395ad2e491ab389b6
dinareda <dina.reda.elsebaey@gmail.com>
2023-06-23 17:19:19 +0300

    validate name and slug and description

--- admin/handelCatogery.php
+++ admin/handelCatogery.php
@@ -3,6 +3,16 @@ session_start();
 include_once "../functions/handelRegisterAndLogin.php";
 include_once "validateCategory.php";
 
+function validateAll($name,$slug,$description,$image)
+{
+  if (validate_name($name) && validate_slug($slug) && validate_description($description) && validate_image($image))
+  {
+    return true;
+  } else
+  {
+    return false;
+  }
+}
     if(isset($_POST['add_category_btn']))
     {
         $name = $_POST['name'] ;
@@ -15,53 +25,39 @@ include_once "validateCategory.php";
         $meta_description = $_POST['meta_description'] ;
         $meta_keywords = $_POST['meta_keywords'] ;
         
-            //extract image details
+        //extract image details
         $img_name=$image['name'];
         $img_type=$image['type'];
         $img_tmp_name=$image['tmp_name'];
         $img_size=$image['size'];
         $img_error=$image['error'];
 
-            //validation image file
-        $extension_array = array('png','PNG', 'jpg','JPG');
-        $img_ext = pathinfo($img_name, PATHINFO_EXTENSION);
-        if ($img_error != 0)
-        {
-            $_SESSION['message'] = 'There was an error uploading the image.';
-            header("Location: addCategory.php");
-        } elseif ($img_size > 2000000)
-        {
-            $_SESSION['message'] = 'Image size exceeds 2MB';
-            header("Location: addCategory.php");
-        } elseif (!in_array($img_ext, $extension_array))
-        {
-            $_SESSION['message'] = 'Invalid file type. Image must be in PNG or JPG format.';
-            header("Location: addCategory.php");
-        } else
-        {
-            // rename image to prevent overirde if 2 image have the same name
-          $random_img_name = uniqid(). "." . $img_ext;
-            // move image from temp to upload folder //
-          move_uploaded_file($img_tmp_name,"upload/$random_img_name");
-            // insert to db
-          $database =new Database();
-          $conn = $database->getConnection();
+        
 
-          $query = "INSERT INTO categories (`name`, `slug`, `description`, `status`, `popular`, `image`, `meta_title`, `meta_description`, `meta_keywords`, `created_at`)
-                  VALUES ('$name', '$slug', '$description', '$status', '$popular', '$random_img_name', '$meta_title', '$meta_description', '$meta_keywords', NOW())";
-          $result = mysqli_query($conn, $query);
-            if($result)
-            { 
-                $_SESSION['message']= 'Data inserted successfully.';
-                header("Location: addCategory.php");
-                exit();
+          if(validateAll($name,$slug,$description,$image)){
+            $database =new Database();
+            $conn = $database->getConnection();
+
+            $query = "INSERT INTO categories (`name`, `slug`, `description`, `status`, `popular`, `image`, `meta_title`, `meta_description`, `meta_keywords`, `created_at`)
+                    VALUES ('$name', '$slug', '$description', '$status', '$popular', '$random_img_name', '$meta_title', '$meta_description', '$meta_keywords', NOW())";
+            $result = mysqli_query($conn, $query);
+              if($result)
+              { 
+                  $_SESSION['message']= 'Data inserted successfully.';
+                  header("Location: addCategory.php");
+                  exit();
+              }else
+              {
+                  $_SESSION['message']= 'something wrong occure';
+                  header("Location: addCategory.php");
+              } 
             }else
             {
-                $_SESSION['message']= 'something wrong occure';
-                header("Location: addCategory.php");
-            } 
-          }
-        $database ->closeConnection();
-    }
+              echo $_SESSION['message'];
+              header("Location: addCategory.php");
+            }
+        }
+        
 
+    $database ->closeConnection();
 ?>
--- admin/validateCategory.php
+++ admin/validateCategory.php
@@ -1,34 +1,72 @@
 <?php
 include_once "handelCatogery.php";
-function validateImage($image)
-{              
-    $img_name=$image['name'];
-    $img_type=$image['type'];
-    $img_tmp_name=$image['tmp_name'];
-    $img_size=$image['size'];
-    $img_error=$image['error'];
+function validate_name($name)
+{
+    if (!empty($name) && ctype_alpha(str_replace(' ', '', $name)))
+    {
+        return true;
+    } else
+    {
+        $_SESSION['message'] = "Please enter a non-empty name without numeric characters.";
+        return false;
+    }
+}
+
+function validate_slug($slug)
+{
+    if (!empty($slug) && preg_match('/^[a-z0-9\-]+$/', $slug))
+    {
+        return true;
+    } else
+    {
+        $_SESSION['message'] = "Please enter a non-empty slug with lowercase alphanumeric characters and hyphens only.";
+        return false;
+    }
+}
+
+function validate_description($description)
+{
+    if (!empty($description) && strlen($description) >= 10)
+    {
+        return true;
+    } else
+    {
+        $_SESSION['message'] = "Please enter a non-empty description with at least 10 characters.";
+        return false;
+    }
+}
+
+function validate_image($image)
+{
+    $img_name = $image['name'];
+    $img_type = $image['type'];
+    $img_tmp_name = $image['tmp_name'];
+    $img_size = $image['size'];
+    $img_error = $image['error'];
     $img_ext = pathinfo($img_name, PATHINFO_EXTENSION);
-     $extension_array = array('png','PNG', 'jpg','JPG');
-    if( $img_size > 2000000){
-        
-        $_SESSION['message']= 'Image size exceeds 2MB';
+    $extension_array = array('png', 'PNG', 'jpg', 'JPG');
+
+    if ($img_error != 0)
+    {
+        $_SESSION['message'] = 'There was an error uploading the image.';
         return false;
-      }
-      //error state
-      elseif($img_error != 0){
-        $_SESSION['message']= 'there is an error state';
+    } elseif ($img_size > 2000000)
+    {
+        $_SESSION['message'] = 'Image size exceeds 2MB';
         return false;
-      }
-      //type
-      elseif (empty($img_name) && !in_array($img_ext, $extension_array)) {
-        $_SESSION['message']= 'Image must be have extension png or jpg';
+    } elseif (!in_array($img_ext, $extension_array))
+    {
+        $_SESSION['message'] = 'Invalid file type. Image must be in PNG or JPG format.';
         return false;
-      }else{
-        
-        // rename image to prevent overirde if 2 image have the same name
-        $random_img_name = uniqid(). "." . $img_ext;
-        return true ;
+    } else
+    {
+        //rename image to prevent override if 2 images have the same name
+        $random_img_name = uniqid() . "." . $img_ext;
+        //move image from temp to upload folder
+        move_uploaded_file($img_tmp_name, "upload/$random_img_name");
+        return $random_img_name;
+    }
 }
-}
+
 
 ?>
